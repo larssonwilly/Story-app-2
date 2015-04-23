433bac1331d2d82df7c3e25adfb803fd1338692e
Willy <larssonwilly@gmail.com>
2015-04-23 18:22:34 +0200

    Created start button that leads to new activity with a text field

--- app/src/main/java/com/example/willy/storyapp2/StoryMode.java
+++ app/src/main/java/com/example/willy/storyapp2/StoryMode.java
@@ -14,8 +14,7 @@ public class StoryMode extends ActionBarActivity {
         setContentView(R.layout.activity_story_mode);
     }
 
-
-    @Override
+   /* @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_story_mode, menu);
@@ -35,7 +34,7 @@ public class StoryMode extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
 
 }
