f5bfc05bccf3833ebbe26d05260146491168cfb8
Willy <larssonwilly@gmail.com>
2015-04-25 17:21:45 +0200

    changed some functionality

--- app/src/main/java/com/example/willy/storyapp2/StoryMode.java
+++ app/src/main/java/com/example/willy/storyapp2/StoryMode.java
@@ -4,14 +4,47 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class StoryMode extends ActionBarActivity {
+
+    private EditText mEditStoryField;
+    private Button mButton;
+    private TextView mStory;
+    private StringBuilder storyText = new StringBuilder("");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_mode);
+
+        mEditStoryField = (EditText) findViewById(R.id.storyEditText);
+        mButton = (Button) findViewById(R.id.sendStoryButton);
+        mStory  = (TextView) findViewById(R.id.theStory);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String inputText = mEditStoryField.getText().toString();
+                Toast.makeText(StoryMode.this, "Send successful", Toast.LENGTH_LONG).show();
+                updateStory(inputText);
+            }
+        });
+
+    }
+
+    public void updateStory(String inputText)   {
+        storyText.append(inputText + " ");
+        setStoryView(storyText);
+    }
+
+    public void setStoryView(StringBuilder storyText)  {
+        String lastWords = storyText.substring(Math.max(0, storyText.length() - 45));
+        mStory.setText(lastWords);
     }
 
    /* @Override
