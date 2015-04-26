e150cbc1fbc326883d1e744c78d45adbdff97036
Willy <larssonwilly@gmail.com>
2015-04-26 13:20:43 +0200

    commented the code, made a clear text method

--- app/src/main/java/com/example/willy/storyapp2/StoryMode.java
+++ app/src/main/java/com/example/willy/storyapp2/StoryMode.java
@@ -2,8 +2,6 @@ package com.example.willy.storyapp2;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,9 +10,11 @@ import android.widget.Toast;
 
 public class StoryMode extends ActionBarActivity {
 
+    //The elements of the activity
     private EditText mEditStoryField;
     private Button mButton;
-    private TextView mStory;
+    private TextView mEndOfStory;
+    //Stringbuilder is a tool for handling strings, we use it for the append method
     private StringBuilder storyText = new StringBuilder("");
 
     @Override
@@ -22,16 +22,20 @@ public class StoryMode extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_mode);
 
+        //initiate the elements
         mEditStoryField = (EditText) findViewById(R.id.storyEditText);
         mButton = (Button) findViewById(R.id.sendStoryButton);
-        mStory  = (TextView) findViewById(R.id.theStory);
+        mEndOfStory = (TextView) findViewById(R.id.theStory);
 
+        //when the "send" button is clicked, we need to update the story, display the last bit of the story, clear the text for new input
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String inputText = mEditStoryField.getText().toString();
                 Toast.makeText(StoryMode.this, "Send successful", Toast.LENGTH_LONG).show();
-                updateStory(inputText);
+                updateStory(inputText); //updates story
+                setStoryView(storyText); //set the "story view", which is the text field containing the last xx characters of the story
+                clearText(); //clears the text for new input
             }
         });
 
@@ -39,35 +43,15 @@ public class StoryMode extends ActionBarActivity {
 
     public void updateStory(String inputText)   {
         storyText.append(inputText + " ");
-        setStoryView(storyText);
     }
 
     public void setStoryView(StringBuilder storyText)  {
-        String lastWords = storyText.substring(Math.max(0, storyText.length() - 45));
-        mStory.setText(lastWords);
+        String lastWords = storyText.substring(Math.max(0, storyText.length() - 60));
+        mEndOfStory.setText(lastWords);
     }
 
-   /* @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_story_mode, menu);
-        return true;
+    public void clearText() {
+        mEditStoryField.setText("");
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }*/
-
 
 }
